1a879176fcc66f977772a6ba61ff25eecf1a8a99
Adrien RIOU <adrien.riou@vo2-group.com>
2021-01-04 16:49:16 +0100

    ALALA

---
+++ assets/js/pages/things/available-things.page.js
@@ -0,0 +1,33 @@
+parasails.registerPage('available-things', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    //…
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    _.extend(this, SAILS_LOCALS);
+
+  },
+  mounted: async function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    clickThing: async function (thingId) {
+      console.log('clicked thing #' + thingId);
+
+      await Cloud.destroyOneThing.with({ id: thingId });
+      _.remove(this.things, { id: thingId });
+      this.$forceUpdate();
+
+    }
+  }
+});
